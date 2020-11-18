c2e8060ff907f4c24beb663141d008c060679bcd
Alen <alenbasar@outlook.com>
2020-11-18 22:03:15 +1100

    Nov 18 last commit

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredient)
+  let newIngredients = Object.keys(props.ingredient)
     .map((igKey) => {
       return [...Array(props.ingredient[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -12,14 +12,14 @@ const Burger = (props) => {
     .reduce((arr, el) => {
       return arr.concat(el);
     }, []);
-  if (transformedIngredients.length === 0) {
-    transformedIngredients = <p>Please start adding ingredients</p>;
+  if (newIngredients.length === 0) {
+    newIngredients = <p>Please start adding ingredients</p>;
   }
-  console.log(transformedIngredients);
+  console.log(newIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      {transformedIngredients}
+      {newIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
