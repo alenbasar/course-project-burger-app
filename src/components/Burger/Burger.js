9f36f7bdd60e5910de92b702d48566d837946413
Alen <alenbasar@outlook.com>
2020-11-21 23:13:08 +1100

    some minor improvements based on research

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -14,14 +14,24 @@ const Burger = (props) => {
     }, []); */
   let newIngredients = Object.keys(props.ingredient)
     .map((igKey) => {
-      return [...Array(props.ingredient[igKey])].map((_, i) => {
+      return [props.ingredient[igKey]].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
     })
     .reduce((arr, el) => {
       return arr.concat(el);
     }, []);
-
+  /*   let newIngredients = Object.keys(props.ingredient).reduce(
+    (acc, igKey) => [
+      ...[
+        ...props.ingredient[igKey].map((_, i) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />;
+        }),
+      ],
+      ...acc,
+    ],
+    []
+  ); */
   if (newIngredients.length === 0) {
     newIngredients = <p>Please start adding ingredients</p>;
   }
